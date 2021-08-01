fd506f4fae20906a7077fb6eb4d0d6bdc4988311
yungukbae <yungukbae@gmail.com>
2021-08-01 15:47:09 +0900

    mod: devDependencies version, add: form.ts

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,17 @@ module.exports = {
         rules:[
             {
                 test: /\.ts$/,
+                include:[path.resolve(__dirname,'src')],
                 use:'ts-loader',
-                include:[path.resolve(__dirname,'src')]
             }
         ]
+    },
+    resolve:{
+        extensions:['.ts','.js']
     },
     output:{
         publicPath:'public',
         filename:'bundle.js',
         path: path.resolve(__dirname, 'public') //file output
-    }
+    },
 }
